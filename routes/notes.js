6b5e326ce00c6a78e4cf802d905e0b199a0c201c
Jamehzz <probableeme@gmail.com>
2022-03-19 13:34:16 -0400

    added routes

---
+++ routes/notes.js
@@ -0,0 +1,34 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request received to post note`);
+    const { title, text } = req.body;
+
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+        };
+
+        readAndAppend(newNote, './db/db.json')
+
+        const response = {
+            status: 'Success',
+            body: newNote,
+        };
+        console.log(response);
+        res.join(response);
+    }
+    else {
+        res.join('Error in adding note')
+    }
+});
+
+module.exports = notes;
